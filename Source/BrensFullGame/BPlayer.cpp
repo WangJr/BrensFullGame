8c93d416602dcea94a33c56f6f986b1c6d8580fc
WangJr <creightonbrendan@gmail.com>
2016-02-04 18:45:59 -0500

    Setting up the BPlayer Class as default Character Class for Game
    -Fixed Mixamo Master (was causing compile error)
    -Set up BPlayer C++ class and Blueprint
    -Set BP_BPlayer as default Pawn class in Game Mode source

---
+++ Source/BrensFullGame/BPlayer.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BrensFullGame.h"
+#include "BPlayer.h"
+
+
+/////////////////////////////////////////////////////////////////////////
+// ABPlayer - Maing class for the player
+
+ABPlayer::ABPlayer()
+{
+	// Create a camera boom (pulls in towards the player if there is a collision)
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->AttachTo(RootComponent);
+	CameraBoom->TargetArmLength = 300.0f; // The camera follows at this distance behind the character	
+	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
+
+												// Create a follow camera
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->AttachTo(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
+	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
+
+	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
+	// are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
+}
+
+// Called to bind functionality to input
+void ABPlayer::SetupPlayerInputComponent(class UInputComponent* InputComponent)
+{
+	Super::SetupPlayerInputComponent(InputComponent);
+
+}
+
